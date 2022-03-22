8e084643ec0cda9d81176b7e249db9413472423f
rajigithu <86732541+rajigithu@users.noreply.github.com>
2022-03-22 17:07:44 -0400

    modified

---
+++ Rajikitchen/file1.js
@@ -0,0 +1,11 @@
+function printreview(){
+
+    var x=document.review.email.value;
+    var y=document.review.ta1.value;
+    //document.write(x+" "+y);
+    alert("Email: "+x+" Comment: "+y );
+    
+    $("#tab1").append('<tr><td style="color:purple";>'+x+'</td><td style="color:red;">'+y+'</td></tr>');
+    
+    }
+    
---
+++ Rajikitchen/file2.js
@@ -0,0 +1,4 @@
+function checkout(){
+    document.write("Thank YOU!!Payment submitted successfully!");
+    window.alert("Thank YOU!!Payment submitted successfully!");
+    }
